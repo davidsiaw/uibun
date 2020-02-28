7e232abf7f65a21b3aa161e51a4790bdc6b958f8
David Siaw <dsiaw@degica.com>
2020-02-28 11:51:23 +0900

    initial commit

---
+++ main.rb
@@ -0,0 +1,131 @@
+# frozen_string_literal: true
+
+require 'pry'
+
+# A compilable token
+class Token
+  def initialize(raw)
+    @raw = raw
+  end
+
+  def type
+    return :literal if @raw.start_with? '「'
+
+    :ident
+  end
+
+  def name
+    return @raw[1..-2] if type == :literal
+
+    @raw
+  end
+end
+
+# The machine
+class Machine
+  attr_accessor :dictionary, :value_stack, :compile_stack
+
+  def initialize
+    @dictionary = {}
+    @value_stack = []
+    @compile_stack = []
+  end
+
+  def run_token(token)
+    return @value_stack.push(token.name) if token.type == :literal
+    raise '見知らぬ名義' unless exists?(token)
+
+    execute(token)
+  end
+
+  def execute(token)
+    entry = @dictionary[token.name]
+    return entry[:proc].call if entry[:type] == :proc
+
+  end
+
+  def exists?(token)
+    @dictionary.key?(token.name)
+  end
+end
+
+# The scanner
+class Scanner
+  def initialize(source)
+    @source = source
+    rewind!
+  end
+
+  def space?(char)
+    !notspace?(char)
+  end
+
+  def notspace?(char)
+    /\s|　|を|と|は|、/.match(char).nil?
+  end
+
+  def rewind!
+    @position = 0
+  end
+
+  def next_token
+    # Move past spaces
+    loop do
+      break if notspace? @source[@position]
+      break if @source[@position].nil?
+
+      @position += 1
+    end
+
+    return nil if @position >= @source.length
+    return literal if @source[@position] == '「'
+
+    ident
+  end
+
+  def literal
+    # Consume literal
+    start_pos = @position
+    loop do
+      break if @source[@position].nil?
+      break if @source[@position] == '」'
+
+      @position += 1
+    end
+
+    @position += 1
+    @source[start_pos...@position]
+  end
+
+  def ident
+    # Consume token
+    start_pos = @position
+    loop do
+      break if space? @source[@position]
+      break if @source[@position].nil?
+
+      @position += 1
+    end
+
+    @source[start_pos...@position]
+  end
+end
+
+source = ARGF.read
+scanner = Scanner.new(source)
+machine = Machine.new
+
+machine.dictionary['示す'] = {
+  type: :proc,
+  proc: Proc.new do
+    puts machine.value_stack.pop
+  end
+}
+
+loop do
+  word = scanner.next_token
+  break if word.nil?
+
+  token = Token.new(word)
+  machine.run_token(token)
+end
