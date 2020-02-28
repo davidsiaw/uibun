2b3e3b32030c927935115da194b768823b26cdfb
David Siaw <dsiaw@degica.com>
2020-02-28 13:49:03 +0900

    functions and vars

--- main.rb
+++ main.rb
@@ -11,6 +11,7 @@ class Token
   def type
     return :literal if @raw.start_with? '「'
     return :comment if @raw.start_with? '※'
+    return :declmark if @raw.end_with? 'は'
 
     :ident
   end
@@ -30,20 +31,49 @@ class Machine
     @dictionary = {}
     @value_stack = []
     @compile_stack = []
+    @compile_mode = false
   end
 
   def run_token(token)
+    return compile(token) if @compile_mode
     return @value_stack.push(token.name) if token.type == :literal
     return if token.type == :comment
+    return switch_to_compile! if token.type == :declmark
     raise '見知らぬ名義' unless exists?(token)
 
     execute(token)
   end
 
+  def compile(token)
+    return switch_off_compile! if token.name == 'です' && token.type != :literal
+
+    @compile_stack.push(token)
+  end
+
+  def switch_to_compile!
+    @compile_mode = true
+  end
+
+  def switch_off_compile!
+    @compile_mode = false
+    name = @value_stack.pop
+    @dictionary[name] = {
+      type: :method,
+      method: @compile_stack
+    }
+    @compile_stack = []
+  end
+
   def execute(token)
     entry = @dictionary[token.name]
     return entry[:proc].call if entry[:type] == :proc
+    return execute_method(entry[:method]) if entry[:type] == :method
+  end
 
+  def execute_method(token_list)
+    token_list.each do |token|
+      run_token(token)
+    end
   end
 
   def exists?(token)
@@ -71,6 +101,17 @@ class Scanner
   end
 
   def next_token
+    consume_spaces!
+
+    return nil if @position >= @source.length
+    return literal if @source[@position] == '「'
+    return comment if @source[@position] == '※'
+    return declaration if @source[@position] == 'は'
+
+    ident
+  end
+
+  def consume_spaces!
     # Move past spaces
     loop do
       break if notspace? @source[@position]
@@ -78,12 +119,13 @@ class Scanner
 
       @position += 1
     end
+  end
 
-    return nil if @position >= @source.length
-    return literal if @source[@position] == '「'
-    return comment if @source[@position] == '※'
+  def declaration
+    @position += 1
+    return 'とは' if @source[@position - 2] == 'と'
 
-    ident
+    'は'
   end
 
   def comment
@@ -93,6 +135,7 @@ class Scanner
     loop do
       break if @source[@position].nil?
       break if @source[@position] == '※'
+
       @position += 1
     end
 
@@ -135,7 +178,14 @@ machine = Machine.new
 machine.dictionary['書く'] = {
   type: :proc,
   proc: lambda do
-    puts machine.value_stack.pop
+    print machine.value_stack.pop
+  end
+}
+
+machine.dictionary['改行する'] = {
+  type: :proc,
+  proc: lambda do
+    puts
   end
 }
 
